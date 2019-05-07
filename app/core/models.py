84c1ebff65ebe452501f4474c27aaacbd192fe96
Spectre <bowo@anakdesa.id>
2019-05-07 13:05:13 +0700

    Update Models

--- app/core/models.py
+++ app/core/models.py
@@ -1,14 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
+from django.contrib.auth.models import AbstractBaseUser, \
+    BaseUserManager, PermissionsMixin
 class UserManager(BaseUserManager):
-    
     def create_user(self, email, password=None, **extra_fields):
         """Create and save a new user"""
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
