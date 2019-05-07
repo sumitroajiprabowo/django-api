1928ccec83e25e4a4ffca21d9c0d738830c31e49
Spectre <bowo@anakdesa.id>
2019-05-07 14:17:39 +0700

    Change Models

--- app/core/models.py
+++ app/core/models.py
@@ -1,13 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, \
-    BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
+
+
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Create and save a new user"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
         return user
+
+    def create_superuser(self, email, password):
+        """Create and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user"""
@@ -19,4 +33,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-
