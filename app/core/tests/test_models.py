b49dbb58d4ab6cd487e94cffcb714248b2debfd1
Spectre <bowo@anakdesa.id>
2019-05-07 14:23:33 +0700

    Change Test Models

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
-class ModelTests(TestCase):  
+class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test Creating a new user with an email is successful"""
         email = 'bowo@anakdesa.id'
